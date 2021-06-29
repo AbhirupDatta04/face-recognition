9eabd4408b8361e8a3729a03160990b1ae8f450e
Abhirup <abhirupdatta4@gmail.com>
2021-06-29 22:31:47 +0530

    Cascade filters loading done

---
+++ Face_recognition_openCV.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 22:24:21 2021
+
+@author: abhir
+"""
+import cv2
+
+face_casc=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_casc=cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+
